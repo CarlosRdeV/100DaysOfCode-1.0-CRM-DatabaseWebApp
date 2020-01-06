f02e80544af02d1768c114e8be3cd5bfbe3b7b8f
Carlos Rivera <carlosrdev@gmail.com>
2020-01-05 19:52:19 -0600

    Update MovieController.java
    Actualizamos nuestros controlador de Movie,
    Ahora hacemos uso de nuestro movieDAO para traer las películas en nuestra base de datos y las agregamos al model.

--- Project/src/com/carlosrdev/springdemo/controller/MovieController.java
+++ Project/src/com/carlosrdev/springdemo/controller/MovieController.java
@@ -1,15 +1,31 @@
 package com.carlosrdev.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.carlosrdev.springdemo.dao.MovieDAO;
+import com.carlosrdev.springdemo.entity.Movie;
 
 @Controller
 @RequestMapping("/movie")
 public class MovieController {
 
+	//need to inject the movie dao
+	@Autowired
+	private MovieDAO movieDAO;
+	
 	@RequestMapping("/list")
 	public String listMovies(Model theModel) {
+		
+		//get the movies from the DAO
+		List<Movie> theMovies = movieDAO.getMovies();
+		
+		//add the movies to the model
+		theModel.addAttribute("movies", theModel);
 		
 		return "list-movies";
 	}
