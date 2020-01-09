fe57fb4899e5ab9df45c50cf73e3422202895f4b
Carlos Rivera <carlosrdev@gmail.com>
2020-01-09 01:17:56 -0600

    Create MembershipDAO.java
    Creamo un nuevo DAO para comprobar el funcionamiento de nuestro pointcut creado para cualquier metodo llamado addAccount()

---
+++ Spring Demo AOP/src/com/carlosrdev/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,9 @@
+package com.carlosrdev.aopdemo.dao;
+
+@Component
+public class MembershipDAO {
+
+	public void addAccount() {
+		System.out.println(getClass() + ": DOING STUFF: ADDING A MEMBERSHIP ACCOUNT");
+	}
+}
