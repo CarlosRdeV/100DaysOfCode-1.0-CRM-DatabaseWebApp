bf94d78c5324afa64edb67a8dd00880871539264
Carlos Rivera <carlosrdev@gmail.com>
2020-01-09 01:41:05 -0600

    Update MyDemoLoggingAspect.java
    modificamos nuestro pointcut expression para que funcione solo con los metodos que tengan un tipo de retorno void y comiencen con "add"

--- Spring Demo AOP/src/com/carlosrdev/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Spring Demo AOP/src/com/carlosrdev/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,7 +11,10 @@ public class MyDemoLoggingAspect {
 	//this is where we add all of our related advices for logging
 	
 	//lets start with an @Before advice
-	@Before("execution(public void add*())")
+
+	//@Before("execution(public void add*())")
+
+	@Before("execution(void add*())")
 	public void beforeAddAccount(){
 		
 		System.out.println("\n=====>>> Executing @Before advice	on AddAccount()");
