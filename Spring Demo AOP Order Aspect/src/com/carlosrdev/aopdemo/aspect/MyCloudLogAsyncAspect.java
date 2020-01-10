0cde03ba02238423d306a48e6f009e078076bf31
Carlos Rivera <carlosrdev@gmail.com>
2020-01-09 23:35:06 -0600

    Creamos clases para cada advice

---
+++ Spring Demo AOP Order Aspect/src/com/carlosrdev/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -0,0 +1,17 @@
+package com.carlosrdev.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyCloudLogAsyncAspect {
+	
+	@Before("forDaoPackageNoGetterSetter()")
+	public void logToCloadAsync(){
+		
+		System.out.println("\n=====>>> Logging to Cloud in async fashion");
+	}
+
+}
