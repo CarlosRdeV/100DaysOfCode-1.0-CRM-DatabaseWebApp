981ea974f0e18c0abd32773d7d67caf1a41febad
Carlos Rivera <carlosrdev@gmail.com>
2020-01-09 21:19:50 -0600

    Update MainDemoApp.java

--- Spring Demo AOP Pointcut Declarations/src/com/carlosrdev/aopdemo/MainDemoApp.java
+++ Spring Demo AOP Pointcut Declarations/src/com/carlosrdev/aopdemo/MainDemoApp.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.carlosrdev.aopdemo.dao.AccountDAO;
 import com.carlosrdev.aopdemo.dao.MembershipDAO;
 
-public class MainDemoApp {
+public class MainDemoApp {	
 
 	public static void main(String[] args) {
 
