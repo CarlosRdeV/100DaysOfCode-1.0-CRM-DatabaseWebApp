b64389b593dfbf1c02237b1e737d0ddb31b48163
Carlos Rivera <carlosrdev@gmail.com>
2020-01-09 22:59:59 -0600

    Update MyDemoLoggingAspect.java
    Creamos los pointcuts para todos los metodos get y set, luego los combinamos para hacer un pointcut que se aplique excluyendo todos los metodos get y set.

--- Spring Demo AOP Pointcut Combo/src/com/carlosrdev/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Spring Demo AOP Pointcut Combo/src/com/carlosrdev/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -16,13 +16,25 @@ public class MyDemoLoggingAspect {
 	@Pointcut("execution(* com.carlosrdev.aopdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
 	
-	@Before("forDaoPackage()")
+	// create a pointcut for getter methods
+	@Pointcut("execution(* com.carlosrdev.aopdemo.dao.*.get*(..))")
+	private void getter() {}
+	
+	// create a pointcut for setter methods
+	@Pointcut("execution(* com.carlosrdev.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+	
+	// creat pointcut: include package ... exclude getter/setters
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {}
+	
+	@Before("forDaoPackageNoGetterSetter()")
 	public void beforeAddAccount(){
 		
 		System.out.println("\n=====>>> Executing @Before advice	on AddAccount()");
 	}
 	
-	@Before("forDaoPackage()")
+	@Before("forDaoPackageNoGetterSetter()")
 	public void performApiAnalytics(){
 		
 		System.out.println("\n=====>>> Performing API analytics");
