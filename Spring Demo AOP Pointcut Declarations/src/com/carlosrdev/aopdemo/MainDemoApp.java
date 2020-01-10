860783d0215433598eb6c9d4bb9f3b1e12fe196c
Carlos Rivera <carlosrdev@gmail.com>
2020-01-09 21:13:02 -0600

    Copy Last Proyect
    Copiamos el proyecto anterior que ya tenemos configurado para hacer uso de AOP

---
+++ Spring Demo AOP Pointcut Declarations/src/com/carlosrdev/aopdemo/MainDemoApp.java
@@ -0,0 +1,39 @@
+package com.carlosrdev.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.carlosrdev.aopdemo.dao.AccountDAO;
+import com.carlosrdev.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+
+		//read spring config java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		//get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO",AccountDAO.class);
+		
+		//get membership bean from spring container
+		MembershipDAO theMembershipDAO = 
+				context.getBean("membershipDAO",MembershipDAO.class);
+		
+		//call the business method
+		Account myAccount = new Account();
+		
+		theAccountDAO.addAccount(myAccount, true);
+		
+		theAccountDAO.doWork();
+		
+		//call the membership bussiness method
+		theMembershipDAO.addAccount();
+		
+		theMembershipDAO.goToSleep();
+		
+		//close context
+		context.close();
+	}
+
+}
