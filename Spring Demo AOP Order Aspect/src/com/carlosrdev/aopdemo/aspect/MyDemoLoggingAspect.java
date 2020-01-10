b9035d38230847c18d6a17543cffc2c7a9abda0d
Carlos Rivera <carlosrdev@gmail.com>
2020-01-09 23:39:18 -0600

    Update MyDemoLoggingAspect.java
    Configuramos correctamente el nombre del paquete para obtener el pointcut y le damos el orden 2

--- Spring Demo AOP Order Aspect/src/com/carlosrdev/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Spring Demo AOP Order Aspect/src/com/carlosrdev/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,18 +2,19 @@ package com.carlosrdev.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 	
 	//this is where we add all of our related advices for logging
 	
 	//lets start with an @Before advice
 	
-	@Before("forDaoPackageNoGetterSetter()")
+	@Before("com.carlosrdev.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccount(){
 		
 		System.out.println("\n=====>>> Executing @Before advice	on AddAccount()");
