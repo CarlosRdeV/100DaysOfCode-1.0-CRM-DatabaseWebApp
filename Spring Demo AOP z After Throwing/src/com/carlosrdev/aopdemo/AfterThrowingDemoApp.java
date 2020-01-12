922508684c55b623902855ab10fc0f936f65317c
Carlos Rivera <carlosrdev@gmail.com>
2020-01-11 20:25:41 -0600

    Create AfterThrowingDemoApp.java
    Creamos una nueva app para correr, agregamos un bloque try catch

---
+++ Spring Demo AOP z After Throwing/src/com/carlosrdev/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,48 @@
+package com.carlosrdev.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.carlosrdev.aopdemo.dao.AccountDAO;
+
+public class AfterThrowingDemoApp {	
+
+	public static void main(String[] args) {
+
+		//read spring config java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		//get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO",AccountDAO.class);
+
+		List<Account> theAccounts = null;
+		
+		//call method to find the accounts
+		try {
+			
+			theAccounts = theAccountDAO.findAccounts();
+			
+		} catch (Exception exc) {
+		
+			System.out.println("\n\nMain Program ... caugh exception: " + exc);
+		
+		}
+		
+		
+		
+		
+		//display the accounts
+		System.out.println("\n\nMain Program: Afterthrowing Demo App");
+		System.out.println("--------");
+		
+		System.out.println(theAccounts);
+		
+		System.out.println("\n");
+	
+		//close context
+		context.close();
+	}
+
+}
