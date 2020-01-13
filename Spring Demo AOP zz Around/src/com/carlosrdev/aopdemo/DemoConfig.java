8bf0d7561276ca37bd388fbe4b33439b5bac53da
Carlos Rivera <carlosrdev@gmail.com>
2020-01-12 18:15:49 -0600

    Create new project for Around Advice

---
+++ Spring Demo AOP zz Around/src/com/carlosrdev/aopdemo/DemoConfig.java
@@ -0,0 +1,14 @@
+package com.carlosrdev.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.carlosrdev.aopdemo")
+public class DemoConfig {
+	
+	
+
+}
