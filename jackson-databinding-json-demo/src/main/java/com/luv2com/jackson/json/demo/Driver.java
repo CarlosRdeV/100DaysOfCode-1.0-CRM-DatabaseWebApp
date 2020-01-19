a0051a3438c764b1cbf2996d0ec00f34d01bee80
Carlos Rivera <carlosrdev@gmail.com>
2020-01-18 23:02:21 -0600

    Update Driver.java
    Actualizamos el archivo que usaremos, ahora será el full

--- jackson-databinding-json-demo/src/main/java/com/luv2com/jackson/json/demo/Driver.java
+++ jackson-databinding-json-demo/src/main/java/com/luv2com/jackson/json/demo/Driver.java
@@ -16,11 +16,12 @@ public class Driver {
 			//read json file and map/convert to Java POJO: 
 			//data/sample-lite.json			
 			Student theStudent = 
-					mapper.readValue(new File("data/sample-lite.json"), Student.class);
+					mapper.readValue(new File("data/sample-full.json"), Student.class);
 			
 			//print first name and last name 
 			System.out.println("First Name: " + theStudent.getFirstName());
 			System.out.println("Last Name: " + theStudent.getLastName());
+			System.out.println("Languages: " + theStudent.getLanguages());
 			
 			
 		} catch (Exception e) {
