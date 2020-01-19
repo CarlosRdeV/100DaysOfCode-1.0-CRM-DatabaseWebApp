5457ec3b298bafceea45e0064e57d7b356d1f72a
Carlos Rivera <carlosrdev@gmail.com>
2020-01-19 16:46:37 -0600

    Update StudentRestController.java
    refactorizamos el metodo antiguo para cargar los datos una sola vez, ademas agregamos un end point nuevo que regresara solo un objeto Student usando el Id seleccionado mediante PathVariable

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,18 +15,33 @@ import com.luv2code.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-
-	//define end point for "/students" - return list of students 
-	@GetMapping("/students")
-	public List<Student> getStudents(){
+	
+	private List<Student> theStudents;
+	
+	//define @PostConstruct to load the student data ... only once!
+	
+	@PostConstruct
+	public void loadDate() {
 		
-		List<Student> theStudents = new ArrayList<>();
+		theStudents = new ArrayList<>();
 		
 		theStudents.add(new Student("Fernando", "Melendez"));
 		theStudents.add(new Student("Karen", "Reyes"));
 		theStudents.add(new Student("Marry", "Martinez"));
 		
+	}
+	
+	//define end point for "/students" - return list of students 
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+				
 		return theStudents;
 	}
 	
+	// define end point for "/students/{studentId}" - retunr student at index
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		return theStudents.get(studentId);
+	}
 }
