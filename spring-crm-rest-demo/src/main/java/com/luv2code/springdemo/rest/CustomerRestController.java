3585b17b4d86c30067b3281b33a8440cf609b639
Carlos Rivera <carlosrdev@gmail.com>
2020-01-21 14:45:52 -0600

    Update CustomerRestController.java
    Se creó el end point necesario para actualizar un objeto que ya existe previamente en la base de datos

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,7 +43,7 @@ public class CustomerRestController {
 		return theCustomer;
 	}
 	
-	// add mapping for POST /customers
+	// add mapping for POST /customers - add new customer
 	@PostMapping("/customers")
 	public Customer addCustomer(@RequestBody Customer theCustomer) {
 		
@@ -50,6 +51,15 @@ public class CustomerRestController {
 		// this is force a save of new item ... instead of update
 		
 		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	// add mapping for PUT /customers - update existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
 		
 		customerService.saveCustomer(theCustomer);
 		
