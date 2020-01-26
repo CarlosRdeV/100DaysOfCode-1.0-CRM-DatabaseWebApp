03066f0e25a71b6fd52569c0379b018e8266d1ae
Carlos Rivera <carlosrdev@gmail.com>
2020-01-25 20:55:21 -0600

    Update for automatic reload
    Agregamos la dependencia "devtools" para automatizar la recarga de nuestro sistema al agregar un nuevo codigo, mientras se ejecuta realizamos la configuración de 2 nuevos end points verificando la funcionalidad de recarga

--- Spring Boot Demo Devtools/src/main/java/com/carlosrdev/springboot/demo/myspringbootcoolapp/rest/FunRestController.java
+++ Spring Boot Demo Devtools/src/main/java/com/carlosrdev/springboot/demo/myspringbootcoolapp/rest/FunRestController.java
@@ -14,4 +14,15 @@ public class FunRestController {
 		return "Hello World!! time on server is: " + LocalDateTime.now();
 	}
 	
+	// expose a new end point for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard 5k!";
+	}
+	
+	// expose a new end point for "fortune"
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day";
+	}
 }
