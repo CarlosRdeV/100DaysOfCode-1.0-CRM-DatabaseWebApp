e45113b806329a285110bbb9b2206fdb23222140
Carlos Rivera <carlosrdev@gmail.com>
2020-01-28 18:20:29 -0600

    Update EmployeeRestController.java
    Agregamos el end-point necesario para eliminar empleados existentes por Id

--- Spring Boot REST CRUD API Project/cruddemo/src/main/java/com/carlosrdev/springboot/cruddemo/rest/EmployeeRestController.java
+++ Spring Boot REST CRUD API Project/cruddemo/src/main/java/com/carlosrdev/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.carlosrdev.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,21 +60,30 @@ public class EmployeeRestController {
 
 		return theEmployee;
 	}
-	
+
 	// add mapping for PUT /employees - update existing employee
-	
+
 	@PutMapping("/employees")
 	public Employee updateEmployee(@RequestBody Employee theEmployee) {
-		
+
 		employeeService.save(theEmployee);
-		
+
 		return theEmployee;
 	}
-	
-	
-	
-	
-	
-	
+
+	// add mapping for DELETE /employees/{employeeId} - delete existing employee
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+
+		Employee theEmployee = employeeService.findById(employeeId);
+
+		if (theEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+
+		employeeService.delete(employeeId);
+
+		return "Deleted employee id - " + employeeId;
+	}
 
 }
